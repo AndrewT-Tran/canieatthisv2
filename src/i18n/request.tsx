c1f38ef4c6bad4a8a2a9f4af0e8cf40766570a50
Andrew T <me@andrewtran-shi.dev>
2025-01-14 07:37:01 -0800

    modified:   src/i18n/request.tsx

--- src/i18n/request.tsx
+++ src/i18n/request.tsx
@@ -1,4 +1,4 @@
-import { getRequestConfig, setRequestLocale } from 'next-intl/server';
+import { getRequestConfig } from 'next-intl/server';
 import { defaultLocale, locales } from './config';
 import messages from '../../messages/index';
 
@@ -7,19 +7,20 @@ const getMessages = async (locale: string) => {
 };
 
 export default getRequestConfig(async (params: any) => {
-    let locale = await setRequestLocale(params.req);
+    // Extract locale from the request headers or set default
+    let locale = params.req.headers['accept-language']?.split(',')[0] || defaultLocale;
+
     console.log('Determined locale:', locale);
-    if (typeof locale !== 'string' || !locales.includes(locale)) {
+
+    if (!locales.includes(locale)) {
         console.warn('Invalid locale detected, falling back to default:', defaultLocale);
         locale = defaultLocale;
     }
-    if (locale === undefined) {
-        locale = defaultLocale;
-    }
+
     return {
         messages: await getMessages(locale),
         timeZone: 'UTC',
         defaultLocale,
-        locales
+        locales,
     };
-}); 
+});
